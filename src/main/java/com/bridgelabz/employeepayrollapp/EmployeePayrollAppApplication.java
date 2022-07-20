8c09ee797fbd5cdc334857dfd60b21c1a005a25b
Yogesh More <iyogesh.more1998@gmail.com>
2022-07-20 20:07:08 +0530

    Added : Refactor The Code.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -3,12 +3,16 @@ package com.bridgelabz.employeepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 @Slf4j
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
-        System.out.println("Welcome To The Employee Payroll App");
+        ApplicationContext context = SpringApplication
+                .run(EmployeePayrollAppApplication.class, args);
+        System.out.println("Welcome to Employee Payroll App.");
+        log.info("Employee Payroll App Started in {} Environment.",
+                context.getEnvironment().getProperty("environment"));
     }
-
 }
